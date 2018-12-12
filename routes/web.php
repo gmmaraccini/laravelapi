d2ebc57585cb088846f8e3f5e444321805e0fa53
Gabriel M Maraccini <gmmaraccini@gmail.com>
2018-12-12 11:54:01 -0200

    Cria as rotas e tira crsf de tudo

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,32 @@
 |
 */
 
+Route::group(['prefix' => 'api'],function(){
+	Route::group(['prefix' => 'user'],function(){
+		Route::get('',function(){
+			return "Devolver a lista de usuarios";
+		});
+
+		Route::get('{id}',function($id){
+			return "Devolve o usuario {$id}";
+		});
+
+		Route::post('',function(){
+			return "Cria um usuario baseado na informacao recebida";
+		});
+
+
+		Route::put('{id}',function($id){
+			return "Atualizar o usuario de ID " . $id;
+		});
+
+		Route::delete('{id}',function($id){
+			return "Deleta o usuario de ID " . $id;
+		});
+	});
+
+});
+
 Route::get('/', function () {
     return "Hello World";
 });
